7c9c909506121d5f0364b4bea15d83ba258def25
Felipe Lang <felipe.lang@ext.vaadin.com>
2018-12-05 11:12:23 -0300

    Change class scope to package

--- src/main/java/com/flowingcode/vaadin/addons/chipfield/Chip.java
+++ src/main/java/com/flowingcode/vaadin/addons/chipfield/Chip.java
@@ -29,20 +29,20 @@ import com.vaadin.flow.component.icon.Icon;
 @SuppressWarnings("serial")
 @Tag("paper-chip")
 @HtmlImport("bower_components/paper-chip/paper-chip.html")
-public class Chip extends Component {
-	
+final class Chip extends Component {
+
 	private String label;
 	private boolean closable;
 	private Icon icon;
 
-    public Chip(String label, boolean closable, Icon icon) {
-    	this.setLabel(label);
-    	this.setClosable(closable);
-    	this.setIcon(icon);
-    }
+	Chip(String label, boolean closable, Icon icon) {
+		this.setLabel(label);
+		this.setClosable(closable);
+		this.setIcon(icon);
+	}
 
-	public Chip(String chipText) {
-		this(chipText,true,null);
+	Chip(String chipText) {
+		this(chipText, true, null);
 	}
 
 	public String getLabel() {
@@ -59,8 +59,9 @@ public class Chip extends Component {
 	}
 
 	public void setClosable(boolean closable) {
-		if (closable)
+		if (closable) {
 			this.getElement().setAttribute("closable", closable);
+		}
 		this.closable = closable;
 	}
 
@@ -69,14 +70,14 @@ public class Chip extends Component {
 	}
 
 	public void setIcon(Icon icon) {
-		if (icon!=null) {
+		if (icon != null) {
 			Span s = new Span();
 			s.setClassName("chip-background");
 			s.getElement().setAttribute("slot", "avatar");
 			s.add(icon);
-			getElement().appendChild(s.getElement());
+			this.getElement().appendChild(s.getElement());
 		}
 		this.icon = icon;
 	}
-    
+
 }
