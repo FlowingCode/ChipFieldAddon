12b224d56994cdbc0b8bebab69731a32c6bb4e00
Nicolas Gonzalez Paz <ngonzalez@flowingcode.com>
2020-10-16 10:35:51 -0300

    Added "footer" with "show source code" and "toggle split layout orientation" checkboxes, with small fontsize. Edited pom to scan test resources.

--- src/test/java/com/flowingcode/vaadin/addons/chipfield/DisabledDemo.java
+++ src/test/java/com/flowingcode/vaadin/addons/chipfield/DisabledDemo.java
@@ -5,11 +5,12 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 @SuppressWarnings("serial")
 public class DisabledDemo extends VerticalLayout {
 	public DisabledDemo() {
-		ChipField<String> chf4 = new ChipField<>("Disabled", "Mercury", "Venus", "Earth");
-		chf4.addSelectedItem("Mercury");
-		chf4.addSelectedItem("Venus");
-		chf4.setDisabled(true);
+		ChipField<String> chf = new ChipField<>("Disabled", "Mercury", "Venus", "Earth");
+		chf.setWidthFull();
+		chf.addSelectedItem("Mercury");
+		chf.addSelectedItem("Venus");
+		chf.setDisabled(true);
 
-		add(chf4);
+		add(chf);
 	}
 }
