5338702094d67924171d43db6139db9c96ae342b
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-03-01 10:14:18 -0300

    test: implement integration tests

--- src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/ViewIT.java
+++ src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/ViewIT.java
@@ -4,11 +4,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertThrows;
 
 import java.util.Arrays;
+import java.util.Collection;
 
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.openqa.selenium.Keys;
 
 public class ViewIT extends AbstractChipfieldTest {
+
+	private static final String LOREM = "Lorem";
+	private static final String IPSUM = "Ipsum";
+	private static final String ADDITIONAL = "Additional";
 
     @Test
 	public void testUpgradedToCustomElement() {
@@ -28,10 +35,63 @@ public class ViewIT extends AbstractChipfieldTest {
 		assertThrows(RuntimeException.class, () -> call("testCallable", false));
 	}
 
+	private Matcher<Collection<String>> isEqualTo(String... values) {
+		return Matchers.equalTo(Arrays.asList(values));
+	}
+
 	@Test
 	public void testCallableFailure2() {
 		// test that the callable mechanism detect failures
 		assertThrows(RuntimeException.class, () -> call("testCallable"));
 	}
+
+	@Test
+	public void testSelectByText() {
+		chipfield.selectByText(LOREM);
+		assertThat(chipfield.getValue(), isEqualTo(LOREM));
+
+		chipfield.selectByText(IPSUM);
+		assertThat(chipfield.getValue(), isEqualTo(LOREM, IPSUM));
+
+		chipfield.sendKeys(Keys.BACK_SPACE);
+		assertThat(chipfield.getValue(), isEqualTo(LOREM));
+
+		chipfield.sendKeys(Keys.BACK_SPACE);
+		assertThat(chipfield.getValue(), Matchers.empty());
+    }
+
+
+	@Test
+	public void testAdditionalItemEnabled() {
+		call("allowAdditionalItems", true);
+
+		chipfield.sendKeys(ADDITIONAL, Keys.ENTER);
+		assertThat(chipfield.getValue(), isEqualTo(ADDITIONAL));
+
+		chipfield.sendKeys(LOREM, Keys.ENTER);
+		assertThat(chipfield.getValue(), isEqualTo(ADDITIONAL, LOREM));
+	}
+
+	@Test
+	public void testAdditionalItemDisabled() {
+		chipfield.sendKeys("Additional", Keys.ENTER);
+		assertThat(chipfield.getValue(), Matchers.empty());
+	}
+
+	@Test
+	public void testReadOnly() {
+
+		chipfield.selectByText(LOREM);
+		assertThat(chipfield.getValue(), isEqualTo(LOREM));
+
+
+		chipfield.sendKeys(Keys.BACK_SPACE);
+		assertThat(chipfield.getValue(), isEqualTo(LOREM));
+
+
+		chipfield.sendKeys(ADDITIONAL, Keys.ENTER);
+		assertThat(chipfield.getValue(), isEqualTo(LOREM));
+	}
+
 
 }
