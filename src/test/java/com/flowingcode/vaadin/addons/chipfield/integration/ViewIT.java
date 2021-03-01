9d3727404d157cb932eedce6368544d98ebcb8c4
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-03-01 09:56:17 -0300

    test: add view for integration tests

--- src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/ViewIT.java
+++ src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/ViewIT.java
@@ -1,19 +1,37 @@
 package com.flowingcode.vaadin.addons.chipfield.integration;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThrows;
 
+import java.util.Arrays;
+
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
-public class ViewIT extends AbstractViewTest {
-
-	public ViewIT() {
-		super("it");
-	}
+public class ViewIT extends AbstractChipfieldTest {
 
     @Test
-	public void upgradedToCustomElement() {
+	public void testUpgradedToCustomElement() {
 		ChipFieldElement chipfield = $(ChipFieldElement.class).first();
 		assertThat(chipfield, hasBeenUpgradedToCustomElement);
-    }
+	}
+
+	@Test
+	public void testCallableSuccess() {
+		// test that the callable mechanism worls
+		call("testCallable", true);
+	}
+
+	@Test
+	public void testCallableFailure() {
+		// test that the callable mechanism detect failures
+		assertThrows(RuntimeException.class, () -> call("testCallable", false));
+	}
+
+	@Test
+	public void testCallableFailure2() {
+		// test that the callable mechanism detect failures
+		assertThrows(RuntimeException.class, () -> call("testCallable"));
+	}
 
 }
