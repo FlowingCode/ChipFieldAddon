5c4623fed567fbf771c3f17d28f41559ad045c78
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-03-02 10:48:57 -0300

    test: add support for client callable interfaces

--- src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/AbstractChipfieldTest.java
+++ src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/AbstractChipfieldTest.java
@@ -1,15 +1,19 @@
 package com.flowingcode.vaadin.addons.chipfield.integration;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.vaadin.flow.component.ClientCallable;
 
@@ -21,38 +25,68 @@ public class AbstractChipfieldTest extends AbstractViewTest {
 		super("it");
 	}
 
-	@Before
-	public void before() {
-		chipfield = $(ChipFieldElement.class).first();
+	@Override
+	public void setup() throws Exception {
+		super.setup();
+		chipfield = $(ChipFieldElement.class).waitForFirst();
 	}
+
 	/**
 	 * Call a {@link ClientCallable} defined on the integration view.
 	 *
 	 * @param callable  the client callable name
 	 * @param arguments arguments to be passed to the callable
-	 * @throws TimeoutException if the callable doesn't complete in 2 seconds.
+	 * @throws TimeoutException if the callable times out (see
+	 *                          {@link WebDriver.Timeouts#setScriptTimeout(long, java.util.concurrent.TimeUnit)
+	 *                          WebDriver.Timeouts}).
 	 * @throws RuntimeException if the callable fails.
 	 */
-	protected final void call(String callable, Object... arguments) {
+	protected final Object call(String callable, Object... arguments) {
 		WebElement view = getDriver().findElement(By.id("view"));
-		String result = "data-callable-result";
+		arguments = Optional.ofNullable(arguments).orElse(new Object[0]);
 
 		StringBuilder script = new StringBuilder();
 		script.append("var view = arguments[0];");
 		script.append("var callable = arguments[1];");
-		script.append("var result = arguments[2];");
-		script.append("view.removeAttribute(result);");
-		script.append("view.$server[callable](...arguments[3])");
-		script.append(" .then(()=>view.setAttribute(result,true))");
-		script.append(" .catch(()=>view.setAttribute(result,false));");
+		script.append("var callback = (result,success) => arguments[3]({result, success});");
+		script.append("view.$server[callable](...arguments[2])");
+		script.append(" .then(result=>callback(result, true))");
+		script.append(" .catch(()=>callback(undefined, false));");
 
-		((JavascriptExecutor) getDriver()).executeScript(script.toString(), view, callable, result, arguments);
+		@SuppressWarnings("unchecked")
+		Map<String, Object> result = (Map<String, Object>) ((JavascriptExecutor) getDriver()).executeAsyncScript(script.toString(), view, callable, arguments);
 
-		new WebDriverWait(getDriver(), 2, 100).until(ExpectedConditions.attributeToBeNotEmpty(view, result));
-		if (!Boolean.parseBoolean(view.getAttribute(result))) {
+		if (!(Boolean) result.get("success")) {
 			throw new RuntimeException(
 					String.format("server call failed: %s(%s)", callable, Stream.of(arguments).map(Object::toString).collect(Collectors.joining(","))));
 		}
+
+		return result.get("result");
+	}
+
+	/**
+	 * Create a TestBench proxy that invokes methods from the interface through a
+	 * client {@link #call}.
+	 */
+	protected final <T> T createCallableProxy(Class<T> intf) {
+		return intf.cast(Proxy.newProxyInstance(intf.getClassLoader(), new Class<?>[] { intf }, new InvocationHandler() {
+			@Override
+			public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+				Object result = call(method.getName(), args);
+
+				if (result == null || method.getReturnType() == Void.TYPE) {
+					return null;
+				}
+
+				if (method.getReturnType() == JsonArrayList.class) {
+					return JsonArrayList.wrapForTestbench((List<?>) result);
+				}
+
+				// this implementation is incomplete.
+				// other types that should be supported are: Double, Integer, Boolean, String, JsonValue
+				throw new ClassCastException(String.format("%s as %s", result.getClass().getName(), method.getReturnType().getName()));
+			}
+		}));
 	}
 
 }
