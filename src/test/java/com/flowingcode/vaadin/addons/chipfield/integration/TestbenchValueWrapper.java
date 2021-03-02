fd72d2b889678ecdd1b4ca3a15a1680aacd58edf
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-03-02 10:48:57 -0300

    test: add support for client callable interfaces

---
+++ src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/TestbenchValueWrapper.java
@@ -0,0 +1,43 @@
+package com.flowingcode.vaadin.addons.chipfield.integration;
+
+import elemental.json.JsonType;
+import elemental.json.JsonValue;
+
+public interface TestbenchValueWrapper extends JsonValue {
+
+	@Override
+	default boolean asBoolean() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	default double asNumber() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	default String asString() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	default JsonType getType() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	default String toJson() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	default boolean jsEquals(JsonValue value) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	default Object toNative() {
+		throw new UnsupportedOperationException();
+	}
+
+}
