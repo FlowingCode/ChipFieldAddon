830ac54f3da07ca1f3a2d6841f8fbbb18d79a478
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2021-03-03 17:48:48 -0300

    chore: add license and author tags

--- src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/rpc/JreJsonArrayList.java
+++ src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/rpc/JreJsonArrayList.java
@@ -1,3 +1,22 @@
+/*-
+ * #%L
+ * ChipField Addon
+ * %%
+ * Copyright (C) 2018 - 2021 Flowing Code
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.flowingcode.vaadin.addons.chipfield.integration.rpc;
 
 import java.util.AbstractCollection;
@@ -12,7 +31,11 @@ import elemental.json.JsonValue;
 import elemental.json.impl.JreJsonArray;
 import lombok.experimental.Delegate;
 
-/** Server-side flavor of {@code JsonArrayList} */
+/**
+ * Server-side flavor of {@code JsonArrayList}
+ *
+ * @author Javier Godoy / Flowing Code
+ */
 @SuppressWarnings("serial")
 class JreJsonArrayList<T> extends JreJsonArray implements JsonArrayList<T> {
 
