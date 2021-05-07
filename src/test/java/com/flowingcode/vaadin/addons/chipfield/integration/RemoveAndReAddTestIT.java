437c72e636aa616f7f90087c143c7ed93c6ba221
Paola De Bartolo <paola.debartolo@gmail.com>
2021-05-07 16:55:56 -0300

    style: apply code formatting

--- src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/RemoveAndReAddTestIT.java
+++ src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/RemoveAndReAddTestIT.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,30 +23,28 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import com.vaadin.flow.component.button.testbench.ButtonElement;
 import org.junit.Test;
 
-import com.vaadin.flow.component.button.testbench.ButtonElement;
-
 /**
- * Test that shows that removing a chipfield with values from the layout and adding it again doesn't lose the chip values.
- * 
+ * Test that shows that removing a chipfield with values from the layout and adding it again doesn't
+ * lose the chip values.
+ *
  * @see https://github.com/FlowingCode/ChipFieldAddon/issues/33
- * 
  */
 public class RemoveAndReAddTestIT extends AbstractRemoveAndReAddTest {
-	
-	@Test
-    public void toggleChipField() {
-        ChipFieldElement chipfield = $(ChipFieldElement.class).id("chipfield");
-        assertTrue(chipfield.isDisplayed());
-        assertEquals(chipfield.getValue().size(), 2);
-        ButtonElement button = $(ButtonElement.class).id("toggle");
-        button.click(); // remove chipfield
-        assertFalse($(ChipFieldElement.class).exists());
-        button.click(); // add chipfield again
-        chipfield = $(ChipFieldElement.class).id("chipfield");
-        assertTrue(chipfield.isDisplayed());
-        assertEquals(chipfield.getValue().size(), 2);        
-    }
 
+  @Test
+  public void toggleChipField() {
+    ChipFieldElement chipfield = $(ChipFieldElement.class).id("chipfield");
+    assertTrue(chipfield.isDisplayed());
+    assertEquals(chipfield.getValue().size(), 2);
+    ButtonElement button = $(ButtonElement.class).id("toggle");
+    button.click(); // remove chipfield
+    assertFalse($(ChipFieldElement.class).exists());
+    button.click(); // add chipfield again
+    chipfield = $(ChipFieldElement.class).id("chipfield");
+    assertTrue(chipfield.isDisplayed());
+    assertEquals(chipfield.getValue().size(), 2);
+  }
 }
--- src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/RemoveAndReAddTestView.java
+++ src/test/java/com/flowingcode/vaadin/addons/chipfield/integration/RemoveAndReAddTestView.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,45 +19,42 @@
  */
 package com.flowingcode.vaadin.addons.chipfield.integration;
 
-import java.util.Arrays;
-
 import com.flowingcode.vaadin.addons.chipfield.ChipField;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
+import java.util.Arrays;
 
-/**
- * Test class for fix for issue https://github.com/FlowingCode/ChipFieldAddon/issues/33
- * 
- */
+/** Test class for fix for issue https://github.com/FlowingCode/ChipFieldAddon/issues/33 */
 @Route("remove-add-test")
 public class RemoveAndReAddTestView extends Div {
-	
-	public RemoveAndReAddTestView() {
-		super.getElement().getStyle().set("padding", "5px");
-		
-		setId("remove-add-test");
-		
-		ChipField<String> chf =
-	        new ChipField<>("Planets",  "Mercury", "Venus", "Earth");
-		chf.setId("chipfield");
-	    chf.setWidthFull();
-	    
-	    chf.addSelectedItem("Mercury");
-	    chf.addSelectedItem("Venus");
-	    chf.setValue(Arrays.asList("Mercury", "Venus"));
-	   
-	    Button toggle = new Button("Toggle chipfield", e -> {
-	    	if (getChildren().anyMatch(chf::equals)) {
-	    		remove(chf);
-	    	} else {
-	    		add(chf);
-	    	}
-	    });	    
-	    
-	    toggle.setId("toggle");
-	    
-	    add(toggle, chf);		
-	}
-	
+
+  public RemoveAndReAddTestView() {
+    super.getElement().getStyle().set("padding", "5px");
+
+    setId("remove-add-test");
+
+    ChipField<String> chf = new ChipField<>("Planets", "Mercury", "Venus", "Earth");
+    chf.setId("chipfield");
+    chf.setWidthFull();
+
+    chf.addSelectedItem("Mercury");
+    chf.addSelectedItem("Venus");
+    chf.setValue(Arrays.asList("Mercury", "Venus"));
+
+    Button toggle =
+        new Button(
+            "Toggle chipfield",
+            e -> {
+              if (getChildren().anyMatch(chf::equals)) {
+                remove(chf);
+              } else {
+                add(chf);
+              }
+            });
+
+    toggle.setId("toggle");
+
+    add(toggle, chf);
+  }
 }
